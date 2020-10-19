18b30d1e8f931405572413545f6f75a725560cf8
vitor-gercov <gercovvitor@gmail.com>
2020-10-19 15:16:54 -0300

    typescript - pt. 2(aula 1)

---
+++ typescript/pt2/alurabank/app/js/controllers/NegociacaoController.js
@@ -0,0 +1,42 @@
+System.register(["../views/NegociacoesView", "../views/MsgView", "../models/Negociacoes", "../models/Negociacao"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var NegociacoesView_1, MsgView_1, Negociacoes_1, Negociacao_1, NegociacaoController;
+    return {
+        setters: [
+            function (NegociacoesView_1_1) {
+                NegociacoesView_1 = NegociacoesView_1_1;
+            },
+            function (MsgView_1_1) {
+                MsgView_1 = MsgView_1_1;
+            },
+            function (Negociacoes_1_1) {
+                Negociacoes_1 = Negociacoes_1_1;
+            },
+            function (Negociacao_1_1) {
+                Negociacao_1 = Negociacao_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this._negociacoes = new Negociacoes_1.Negociacoes();
+                    this._negociacoesView = new NegociacoesView_1.NegociacoesView('#negociacoesView');
+                    this._msgView = new MsgView_1.MsgView('#msgView');
+                    this._inputData = $('#data');
+                    this._inputQuantidade = $('#quantidade');
+                    this._inputValor = $('#valor');
+                    this._negociacoesView.update(this._negociacoes);
+                }
+                adiciona(event) {
+                    event.preventDefault();
+                    const negociacao = new Negociacao_1.Negociacao(new Date(this._inputData.val().replace('-', ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    this._negociacoes.adiciona(negociacao);
+                    this._negociacoesView.update(this._negociacoes);
+                    this._msgView.update('Negociação adicionada com sucesso!');
+                }
+            };
+            exports_1("NegociacaoController", NegociacaoController);
+        }
+    };
+});
---
+++ typescript/pt2/alurabank/app/ts/views/View.ts
@@ -0,0 +1,16 @@
+export abstract class View<T> {
+        
+        private _elemento: JQuery
+
+        constructor(seletor: string) {
+
+            this._elemento = $(seletor)
+        }
+
+        update(model: T): void {
+
+            this._elemento.html(this.template(model))
+        }
+
+        abstract template(model: T): string 
+    }
